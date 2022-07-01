09b85c74910be7176d93bf572065790d04040d16
Prachi95 <Prachi95@users.noreply.github.com>
2022-07-01 22:58:04 +0530

    Added optimal solution

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -1,9 +1,36 @@
 //PROBLEM -> https://leetcode.com/problems/valid-anagram/
 
+var isAnagram = function(s, t) {
+    return solution2(s,t)
+};
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+const solution2 = (s,t) => {
+    
+    let alphabetArray = []
+    for(let i=0;i<26;i++) {
+        alphabetArray[i] = 0
+    }
+    for(let c of s) {
+        alphabetArray[c.charCodeAt() - 'a'.charCodeAt()] += 1
+    }
+    
+    for(let c of t) {
+        alphabetArray[c.charCodeAt() - 'a'.charCodeAt()] -= 1
+    }
+   
+    for(let i=0;i<26;i++)  {
+        if(alphabetArray[i] !== 0) return false
+    }
+    
+    return true
+}
+
+
 // Time Complexity -> O(n)
 // Space Complexity -> O(n)
-
-var isAnagram = function(s, t) {
+const solution1 = (s,t) => {
     //base case, check if 2 strings have same length, else return 
     if(s.length != t.length) return false
     
@@ -31,7 +58,7 @@ var isAnagram = function(s, t) {
     
     //check if both has same char length
     return compare(sMap, tMap)
-};
+}
 
 const compare = (map1, map2) => {
     if(map1.length !== map2.length) return false
